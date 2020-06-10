2c76596d13c4f729aebe42a20bbad606f8351bb3
John Alexis Munera <alexismunera98@gmail.com>
2020-06-09 21:49:53 -0500

    Variable types

---
+++ tipos/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Tipos de datos
+    // i8, i16, i32, i64, i128 -> CON signo ( - or + )
+    // u8, u16, u32, u64, u128 -> SIN signo ( - or + )
+    let numero_uno: i8 = -10;
+    let numero_dos: u8 = 10;
+
+    // Char
+    let caracter = 'a';
+
+    // float
+    let real = 12.5; //f64
+
+    // booleanos
+    let booleano: bool = false;
+
+    println!("{} {} {} {} {}", numero_uno, numero_dos, caracter, real, booleano)
+}
